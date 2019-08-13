930cdca54406c4faa3877952718e8b6f5b9db68f
gatakaba <m07112@gmail.com>
2019-08-13 12:27:14 +0900

    update

--- src/times.py
+++ src/times.py
@@ -7,24 +7,22 @@ import sqlite3
 class Times:
     def __init__(self, db_path="times.sqlite"):
         self.conn = sqlite3.connect(db_path)
+        self._create_table()
 
     def _create_table(self):
         sql = """CREATE TABLE IF NOT EXISTS times
-             (id INTEGER PRIMARY KEY,
+             (id INTEGER PRIMARY KEY AUTOINCREMENT,
               message TEXT,
               time TEXT)"""
         self.conn.execute(sql)
         self.conn.commit()
 
-    def post(self, content):
-        message = Message(content)
+    def post(self, message: str):
+        sql = """INSERT INTO times (time,message) values(?,?)"""
+        self.conn.execute(sql, ("time", message))
+        self.conn.commit()
 
     def recall(self, n):
-        pass
-
-
-# @dataclasses.dataclass
-class Message:
-    def __init__(self, message: str, dt: datetime):
-        self.dt = dt
-        self.message = message
+        sql = """SELECT time,message FROM times"""
+        r = self.conn.execute(sql)
+        print(r)
